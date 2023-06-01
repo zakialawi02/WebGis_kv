430434015e3aa5822bd0f0759076f433117cc63a
zakialawi02 <zaki.alawi22@gmail.com>
2023-06-01 16:50:16 +0700

    fix minor error

--- app/Views/admin/updateKafe.php
+++ app/Views/admin/updateKafe.php
@@ -294,10 +294,10 @@
             </main><!-- End #main -->
 
         </div>
-        <!-- FOOTER -->
-        <?= $this->include('_Layout/_template/_admin/footer'); ?>
-
     </div>
+    <!-- FOOTER -->
+    <?= $this->include('_Layout/_template/_admin/footer'); ?>
+
 
     <!-- Vendor JS Files -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
